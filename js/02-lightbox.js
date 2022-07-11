643dd76005f8d4c8ee1478a98dde32782d6de121
dimon4uk1990 <mexal07@ukr.net>
2022-07-11 16:54:15 +0300

    Розмітка 2 завдання

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+
+
+function createGalleryCardsMarkup(pictures) {
+    return pictures.map(({ preview, original, description }) => {
+        return `
+        <li>
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+            src="${preview}"
+            alt="${description}" />
+        </a>
+        </li>`;
+    })
+        .join('');
+    
+}
+console.log(galleryMarkup)
